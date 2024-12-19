5125eec8069d09ccb12ddeaa56bb15e827c995ff
Chitdanai <gamerzfire2215@gmail.com>
2024-12-19 13:38:19 +0700

    w

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP test</title>
+    </head>
+    <body>
+        <?php
+            echo "hello world<br>";
+            print("hello world<br>");
+            print_r("hello world<br>");
+            printf("hello world<br>");
+            var_dump("hello world<br>");
+        ?>
+    
+    </body>
+</html>
